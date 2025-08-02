da49bdf19fc9c2dccbd462dc4f1768ad0691de35
Greeshma93980 <workspace93980@gmail.com>
2025-08-02 12:17:10 +0530

    third update

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
 app=Flask(__name__)
 @app.route("/home")
 def home():
     return "Home page"
 @app.route("/")
+def index():
+    return render_template("index.html")
 def index():
     return "Welcome to Flask"
 if __name__=="__main__":
